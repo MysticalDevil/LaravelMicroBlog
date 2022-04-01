5e25b2f9df54a2bfdcc1debd2c4bd008ff21b76f
MysticalDEvil <jinpen7981@outlook.com>
2022-04-01 22:28:20 +0800

    init style

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
 
 @section('content')
-  <h1>主页</h1>
+  <div class="bg-lighter p-3 p-sm-5 rounded">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+      Now you see <a href="https://laravel.com">Laravel</a> demo home page.
+    </p>
+    <p>
+      It all starts here.
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success"  href="#" role="button">现在注册</a>
+    </p>
+  </div>
 @stop
