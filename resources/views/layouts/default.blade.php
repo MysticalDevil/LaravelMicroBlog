81f64e52df9a2354fbc3816ad055a994e50aebd8
MysticalDEvil <jinpen7981@outlook.com>
2022-04-02 22:18:50 +0800

    Create session

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,16 +1,13 @@
 <!doctype html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title', 'Micro Blog')</title>
   <link rel="stylesheet" href="{{ mix('css/app.css') }}"/>
 </head>
+
 <body>
   @include('layouts._header')
-
+  <br/>
   <div class="container">
     <div class="offset-md-1 col-md-10">
       @include('shared._messages')
